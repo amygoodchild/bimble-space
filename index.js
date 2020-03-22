05074308d80fffa260dbd77e75605c03ee166aee
Amy Goodchild <amy@amygoodchild.com>
2020-03-22 23:03:07 +0000

    first upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Set public path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home page
+app.get('/', function (req, res) {
+  res.render('home', { title: 'home' })
+})
+
+// Toy pages
+app.get('/spirograph', function (req, res) {
+  res.render('toy', { title: 'spirograph' })
+})
+
+// Content Pages
+app.get('/about', function (req, res) {
+  res.render('page', { title: 'about' })
+})
+
+app.get('/contact', function (req, res) {
+  res.render('page', { title: 'contact' })
+})
+
+
+app.listen(3000, function(){
+  console.log('Server is running');
+});
---
+++ public/scripts/mainscripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $("#menuButton").click(function(){
+    $("#menu").toggleClass('menuOpen')
+    $("#menuButton").toggleClass('menuButtonOpen')
+  });
+
+});
