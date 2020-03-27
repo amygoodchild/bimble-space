e9bae19537cda8c8648c8627e3872f2c6eccfcb4
Amy Goodchild <amy@amygoodchild.com>
2020-03-27 17:25:54 +0000

    mobile styles

--- public/scripts/mainscripts.js
+++ public/scripts/mainscripts.js
@@ -56,13 +56,13 @@ $(document).ready(function(){
     $(this).css("top", Math.floor((Math.random() * 100) + 1)+"vh");
   });
 
-  $(".shootingstar").css("left", "-150px").css("top", "40px").delay(20000);
+  $(".shootingstar").css("left", "-250px").css("top", "40px").delay(20000);
 
   function doSomething() {}
     (function loop() {
       var rand = Math.round(Math.random() * (20000 - 5000)) + 20000;
       console.log(rand);
-      //$(".shootingstar").animate({left: "4000px", top: "1500px", opacity: "0"}, 1500).animate({left: "-150px", top: "40px", opacity: "0.2"}, 1);;
+      $(".shootingstar").animate({left: "3900px", top: "1500px", opacity: "0"}, 1500).animate({left: "-250px", top: "40px", opacity: "0.2"}, 1);;
       setTimeout(function() {
               doSomething();
               loop();
@@ -73,13 +73,13 @@ $(document).ready(function(){
 
 
 
-  $(".shootingstar2").css("left", "-150px").css("top", "240px").delay(28000);
+  $(".shootingstar2").css("left", "-250px").css("top", "240px").delay(28000);
 
   function doSomething() {}
     (function loop() {
       var rand = Math.round(Math.random() * (20000 - 5000)) + 20000;
       console.log(rand);
-      //$(".shootingstar2").animate({left: "4000px", top: "1700px", opacity: "0"}, 1500).animate({left: "-150px", top: "240px", opacity: "0.2"}, 1);;
+      $(".shootingstar2").animate({left: "3900px", top: "1700px", opacity: "0"}, 1500).animate({left: "-250px", top: "240px", opacity: "0.2"}, 1);;
       setTimeout(function() {
               doSomething();
               loop();
