68210b7612cc9dce901981a9944bf25c79e83e0a
Amy Goodchild <amy@amygoodchild.com>
2020-04-06 22:35:36 +0100

    favicon

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Home page
 app.get('/', function (req, res) {
-  res.render('home', { title: 'home' })
+  res.render('home', { title: 'Home' })
 })
 
 // Toy pages
 app.get('/spirograph', function (req, res) {
-  res.render('spirograph', { title: 'spirograph', theScripts: ["buttons", "buttonsmobile", "colorbuttons", "colorbuttonsmobile"]})
+  res.render('spirograph', { title: 'Spirograph', theScripts: ["buttons", "buttonsmobile", "colorbuttons", "colorbuttonsmobile"]})
 })
 
 // Content Pages
 app.get('/about', function (req, res) {
-  res.render('page', { title: 'about' })
+  res.render('page', { title: 'About' })
 })
 
 app.get('/contact', function (req, res) {
-  res.render('page', { title: 'contact' })
+  res.render('page', { title: 'Contact' })
 })
 
 app.listen(process.env.PORT || 3000, function(){
