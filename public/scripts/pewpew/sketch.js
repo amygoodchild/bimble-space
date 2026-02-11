4bd6ff809a32d011d305ec705fe67653781246d1
Amy Goodchild <amy@amygoodchild.com>
2026-02-11 11:41:28 +0000

    adjust flock settings

--- public/scripts/pewpew/sketch.js
+++ public/scripts/pewpew/sketch.js
@@ -140,13 +140,13 @@ const pewpewSketch = ( p ) => {
   p.forceSlider;
 
   // flock variables
-  p.perceptionRadius = 150;
+  p.perceptionRadius = 75;
   p.desiredSeparation = 50;
   p.separationAmount = 1.2;
   p.separationDistanceAmount = 30;
   p.alignAmount = 2.1;
   p.cohesionAmount = 0.3;
-  p.forceAmount = 0.8;
+  p.forceAmount = 1.1;
 
   p.dragLength = 0;
   p.drawCounter = 0;
